027491e996f72d8ea052fd8ce9cfe98b561b460d
Alejandro Gálvez <alexgalvez8292@outlook.com>
2015-10-09 14:43:04 +0200

    Server pt1

---
+++ servidor/src/main/java/cursoangularjs/jackson/JsonTransformer.java
@@ -0,0 +1,7 @@
+package cursoangularjs.jackson;
+
+/**
+ * Created by alex on 09/10/15.
+ */
+public interface JsonTransformer {
+}
---
+++ servidor/src/main/java/cursoangularjs/spring/controller/PruebaController.java
@@ -0,0 +1,7 @@
+package cursoangularjs.spring.controller;
+
+/**
+ * Created by alex on 09/10/15.
+ */
+public class PruebaController {
+}
